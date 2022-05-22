d1c3370b4a4f82921116d714d03644488c9ae2e1
ESRAAMUHAMMED4 <103907629+ESRAAMUHAMMED4@users.noreply.github.com>
2022-05-22 17:47:02 +0200

    updates

--- src/app.js
+++ src/app.js
@@ -1,74 +1,38 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+const publicF = path.join(__dirname, "../public");
+app.use(express.static(publicF));
+const hbs = require("hbs");
+app.set("view engine", "hbs");
+const vPath = path.join(__dirname, "../templates/views");
+app.set("views", vPath);
+const request = require("request");
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const path = require('path')
- const publicF = path.join(__dirname,'../public')
-app.use(express.static(publicF))
-const hbs = require('hbs')
- app.set('view engine', 'hbs');
-const vPath = path.join(__dirname,'../templates/views')
-app.set('views',vPath)
-const request = require('request')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
- 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
-// app.get('/index',(req,res)=>{
-//   res.render('index',{
-// people: [
-//  "esraa",
-//  "asmaa",
-//  "nour"
-// ],
-// users :{
-//   username:"esraaM",
-//   age:23
-// }
-//   });
-// });
-
-
-
- 
-  
- 
- 
- 
- 
- 
-const url= 'https://newsapi.org/v2/everything?q=tesla&from=2022-04-22&sortBy=publishedAt&apiKey=a66320e2c31d46f59ba9022e1e27404d'
-request({url,json:true},(err,res)=>{
-
-  if(err){
-    return console.log(err)
+const url =
+  "https://newsapi.org/v2/everything?q=tesla&from=2022-04-22&sortBy=publishedAt&apiKey=a66320e2c31d46f59ba9022e1e27404d";
+request({ url, json: true }, (err, res) => {
+  if (err) {
+    return app.get("/index", (req, res) => {
+      res.send("error has occuered");
+    });
   }
-     const articles =  res.body.articles
-//    publish: "This Article Was Published By "+res.body.articles[0].author ,
-//   title:' The Title Of This Article Is ' + res.body.articles[0].title  ,
-//   discription :' And Description includes ' + res.body.articles[0].description ,
-//  content: ' The Content Of This Article is ' + res.body.articles[0].content,
 
-app.get('/index',(req,res)=>{
-  res.render( 'index',{
-          articles
+  const articles = res.body.articles;
 
-  }) 
-
-}) 
-  console.log(articles)
- 
-
-})
-
-
+  app.get("/index", (req, res) => {
+    res.render("index", {
+      articles,
+    });
+  });
+  // console.log(articles)
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
